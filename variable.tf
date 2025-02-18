822cb91ab71fe4196645540bffc6e697c3d2ae7f
KrithikJoshi <117383620+KrithikJoshi@users.noreply.github.com>
2025-02-18 13:51:56 +0530

    added variable

--- variable.tf
+++ variable.tf
@@ -10,3 +10,9 @@ variable "ec2_instance_type" {
     default = "t2.micro"
   
 }
+
+variable "ec2_ami" {
+    description = "Value of the AMI ID for the EC2 instance"
+    type = string
+    default = "ami-03b8adbf322415fd0"  
+}
